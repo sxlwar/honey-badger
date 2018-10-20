e118f0283bd583dc86267c144df3834dd47755eb
shaoxuelei <sxlwar@163.com>
2018-10-21 02:04:55 +0800

    odify host,port config

--- src/shared/config/config.service.ts
+++ src/shared/config/config.service.ts
@@ -38,6 +38,7 @@ export class ConfigService {
             DATABASE: Joi.string(),
             DATABASE_USER: Joi.string(),
             DATABASE_PASSWORD: Joi.string(),
+            HOST: Joi.string(),
         });
 
         const { error, value: validatedEnvConfig } = Joi.validate(envConfig, envVarsSchema);
@@ -53,14 +54,8 @@ export class ConfigService {
         return this.envConfig[key];
     }
 
-    /**
-     * Method to return specific config variable using the constants from config.constant.ts.
-     * Using this will help us get rid of: process.env.<VAR_NAME> || config.get('<var_name_path>') all over our code.
-     * @param {string} name
-     * @returns {string | number}
-     */
     getConfigVariable(name: string): string {
-        return process.env[name] || get(name);
+        return this.envConfig[name] || get(name);
     }
 
     get isDevelopment(): boolean {
