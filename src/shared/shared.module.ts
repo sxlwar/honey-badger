2998ee4102b6624eb687d65b62a8d3c3d4c95081
shaoxuelei <sxlwar@163.com>
2018-10-27 18:03:01 +0800

    stupid path

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,14 +2,12 @@ import { Global, Module } from '@nestjs/common';
 
 import { ConfigService } from './config/config.service';
 
-const ENV_PATH = process.cwd() + '/config/' + process.env.NODE_ENV + '.env';
-
 @Global()
 @Module({
     providers: [
         {
             provide: ConfigService,
-            useValue: new ConfigService(ENV_PATH),
+            useValue: new ConfigService(`config/${process.env.NODE_ENV}.env`),
         },
     ],
     exports: [ConfigService],
