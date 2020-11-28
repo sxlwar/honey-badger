01bf0dc7b0a76e42dc0590a4ef79aa5c694120c2
sxlwar <sxlwar@163.com>
2020-11-28 19:50:13 +0800

    config: ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    apps: [
+        {
+            name: 'honey-badger',
+            script: './dist/main.js',
+            cwd: '/blog/source/dist',
+            watch: true,
+            env: {
+                COMMON_VARIABLE: 'true',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+
+    deploy: {
+        production: {
+            user: 'root',
+            host: ['chtoma.com'],
+            port: '22',
+            ref: 'origin/master',
+            repo: 'git@github.com:sxlwar/honey-badger.git',
+            path: '/blog',
+            ssh_options: 'StrictHostKeyChecking=no',
+            env: {
+                NODE_ENV: 'production',
+            },
+            'pre-deploy-local': 'npm run prestart:prod',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+        },
+    },
+};
